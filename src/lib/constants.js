ca9a16487db6210303d4d0cd6443b94ee7884867
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-01-02 13:29:01 +0100

    added my projects

---
+++ src/lib/constants.js
@@ -0,0 +1,5 @@
+export const PROJECTS_URL =
+  "https://b7ficnxh.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22post%22%5D";
+
+export const IMAGE_BASE_URL =
+  "https://cdn.sanity.io/images/b7ficnxh/production/";
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,3 +1,58 @@
+import "./Projects.scss";
+import { useEffect, useState } from "react";
+import { IMAGE_BASE_URL, PROJECTS_URL } from "../../lib/constants";
+import {
+  Card,
+  CardMedia,
+  CardActionArea,
+  CardContent,
+  Typography,
+} from "@mui/material";
 export default function Projects() {
-  return <>Mine prosjekter</>;
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch(PROJECTS_URL)
+      .then((response) => response.json())
+      .then((result) => {
+        setProjects(result.result);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const createImageUrl = (imageUrl) => {
+    const fixUrl = imageUrl.replace("image-", "").replace("-jpg", ".jpg");
+    return IMAGE_BASE_URL + fixUrl;
+  };
+
+  const renderProjects = () => {
+    return projects.map((project) => {
+      return (
+        <>
+          <Card sx={{ maxWidth: 345 }} className="card">
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={createImageUrl(project.images[0].asset._ref)}
+                alt="green iguana"
+              />
+              <CardContent className="cardContent">
+                <Typography gutterBottom variant="h5" component="div">
+                  {project.title}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </>
+      );
+    });
+  };
+  return (
+    <>
+      <h1>Mine Prosjekter</h1>
+      <div className="cardContainer">{renderProjects()}</div>
+    </>
+  );
 }
